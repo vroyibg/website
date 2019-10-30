1e02f3223b3044f6301d2390a02afde6383f2a49
vroyibg <vroyibg@gmail.com>
2019-10-30 22:12:02 +0700

    Updates

---
+++ precache-manifest.188477f9fe8a72dfd88ea4db40dde86c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad18e58d0391c93b71c9a8aa102e521",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "1660f90c6cbe817204c7",
+    "url": "/website/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b0fb120175ca89f844b7",
+    "url": "/website/static/js/2.b898528c.chunk.js"
+  },
+  {
+    "revision": "1660f90c6cbe817204c7",
+    "url": "/website/static/js/main.95dc9a55.chunk.js"
+  },
+  {
+    "revision": "64377c2dea863a1783ef",
+    "url": "/website/static/js/runtime-main.4cafdc12.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/website/static/media/logo.25bf045c.svg"
+  }
+]);
